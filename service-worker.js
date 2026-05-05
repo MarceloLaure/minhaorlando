498d339974cc9ffc8cdb12021b39510607c124d8
Marcelo Laure <marceloboys@gmail.com>
2026-05-05 00:25:14 -0300

    Adiciona shows/desfiles na home + sobe layout + restaura park IDs

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Service Worker - Minha Orlando PWA
 // Versão do cache - mude esse número quando atualizar arquivos
-const CACHE_VERSION = 'v47';
+const CACHE_VERSION = 'v48';
 const CACHE_NAME = `minha-orlando-${CACHE_VERSION}`;
 
 // Arquivos do "app shell" - cacheados na instalação
